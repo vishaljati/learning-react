57cedacf207e9269c4aa60ab06503540aa72124d
vishaljati <jativishal120@gmail.com>
2025-11-20 17:49:07 +0530

    complete the setup of scriptblog

---
+++ 12mega-blog/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+
+
+
+// Collections --> Tables
+// Attributes   --> Columns
+// Documents --> Rows
+
+function App() {
+
+
+  return (
+ <>
+ <h1>Blog App</h1>
+ </>
+  )
+}
+
+export default App
