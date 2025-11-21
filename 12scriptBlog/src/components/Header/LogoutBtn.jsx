ed31ed4cbf01fdeea829fd479cceeba568d805a5
vishaljati <jativishal120@gmail.com>
2025-11-21 17:14:14 +0530

    Add Footer and Header component and index file of comp

---
+++ 12scriptBlog/src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {logout} from "../../features/authSlicer"
+import authService from "../../appwrite/auth"
+import {useDispatch} from 'react-redux'
+
+function LogoutBtn() {
+     const dispatch=useDispatch()
+
+     const logoutHandler=()=>{
+          authService.logOut()
+           .then(()=>dispatch(logout()) )
+           .catch((err)=>console.log(err))  
+     }
+
+  return (
+    <button className='inline-bock px-6 py-2
+     duration-200 hover:bg-blue-100 rounded-full'>
+     Logout </button>
+  )
+}
+
+export default LogoutBtn
--- 12scriptBlog/src/components/index.js
+++ 12scriptBlog/src/components/index.js
@@ -1,7 +1,13 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Logo from "./Logo/Logo";
+import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn"
 
 export {
     Header,
-    Footer 
+    Footer ,
+    Logo,
+    Container,
+    LogoutBtn,
 }
