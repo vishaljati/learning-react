1ba1f6542bec5ed18d16fb11a645793ab89ca05c
vishaljati <jativishal120@gmail.com>
2025-11-21 17:48:08 +0530

    Add  Button and input components

---
+++ 12scriptBlog/src/components/Button/Button.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Button(
+    children,
+    className='',
+    type='button',
+    bgColor='bg-blue-600',
+    textColor='text-white',
+    ...props
+) {
+  return (
+    <button type={type} className={`px-4 py-2 rounded-lg ${bgColor} ${textColor}
+     ${className}`}{...props}>
+        {children}
+    </button>
+  )
+}
+
+export default Button
--- 12scriptBlog/src/components/index.js
+++ 12scriptBlog/src/components/index.js
@@ -1,8 +1,12 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Logo from "./Logo/Logo";
-import Container from "./container/Container";
+import Container from "./Container/Container";
 import LogoutBtn from "./Header/LogoutBtn"
+import Button from "./Button/Button"
+import Input from "./Input/Input"
+
+
 
 export {
     Header,
@@ -10,4 +14,8 @@ export {
     Logo,
     Container,
     LogoutBtn,
+    Button,
+    Input
+
+
 }
