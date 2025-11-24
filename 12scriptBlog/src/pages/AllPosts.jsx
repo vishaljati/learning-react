498812842bb632052aa0bebfc86ac6234d63a49d
vishaljati <jativishal120@gmail.com>
2025-11-24 18:02:32 +0530

    Add AddPost, AllPosts, Login, and Signup components

---
+++ 12scriptBlog/src/pages/AllPosts.jsx
@@ -0,0 +1,32 @@
+import React,{useState,useEffect} from 'react'
+import appwriteService from "../appwrite/appwriteConfig"
+import {Container,Postcard} from "../components"
+
+
+function AllPosts() {
+    const [posts,setPosts]=useState([])
+    useEffect(()=>{
+        appwriteService.getAllPosts([])
+        .then((posts)=>{
+            if (posts) {
+               setPosts(posts.documents)
+            }
+        })
+    },[])
+
+  return (
+    <div className='w-full py-8'>
+        <Container>
+            <div className='flex flex-wrap'>
+                {posts.map((post)=>(
+                    <div key={post.$id} className='p-2 w-1/4'>
+                        <Postcard {...post}/>
+                    </div>
+                )) }
+            </div>
+        </Container>
+    </div>
+  )
+}
+
+export default AllPosts
